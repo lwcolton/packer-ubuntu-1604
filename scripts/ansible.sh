143a18405e8b102ec6b7a9b8746f2cf4ce86b153
colton <lwcolton@gmail.com>
2017-03-31 19:04:48 -0500

    fix initial issues

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -1,10 +1,3 @@
 #!/bin/bash -eux
 
-# Install Ansible repository.
-apt -y update && apt-get -y upgrade
-apt -y install software-properties-common
-apt-add-repository ppa:ansible/ansible
-
-# Install Ansible.
-apt -y update
-apt -y install ansible
+sudo pip3 install git+ssh://git@github.com:lwcolton/ansible.git@v2.2.2.0-1

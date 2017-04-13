89940181da788e1de8cf44d4c018fcd872d1dc67
colton <lwcolton@gmail.com>
2017-04-12 19:47:35 -0500

    Build docker with better packages

--- bin/build_docker.sh
+++ bin/build_docker.sh
@@ -7,6 +7,6 @@ qemu-img convert -O raw output-virtualbox-iso/packer-ubuntu-16.04-amd64-disk1.vm
 mkdir -p $DISK_MOUNT
 sudo umount $DISK_MOUNT || true
 sudo mount -o offset=$((2048 * 512)) output-virtualbox-iso/docker.raw $DISK_MOUNT
-sudo rm -rf $DISK_MOUNT/boot $DISK_MOUNT/home
+sudo rm -rf $DISK_MOUNT/boot
 sudo tar -c -C $DISK_MOUNT . | docker import - ubuntu-16-base
 sudo umount $DISK_MOUNT
--- scripts/minimal-packages.sh
+++ scripts/minimal-packages.sh
@@ -1,7 +1,8 @@
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y aptitude ubuntu-minimal
-sudo DEBIAN_FRONTEND=noninteractive aptitude markauto -y '~i!~nubuntu-minimal ~i!~nca-certificates -~i!~napt-transport-https ~i!~napt ~i!~nopenssl ~i!~ntime ~i!~nopenssh-server'
+sudo DEBIAN_FRONTEND=noninteractive aptitude markauto -y '~i!~nubuntu-minimal ~i!~nca-certificates -~i!~napt-transport-https ~i!~napt ~i!~nopenssl ~i!~ntime ~i!~nopenssh-server ~i!~nbuild-essential ~i!~ncurl'
 sudo DEBIAN_FRONTEND=noninteractive apt-get purge -y --auto-remove aptitude
+sudo pip install --upgrade pip
 if [ "$BUILD_TYPE" == "docker" ]; then
   sudo DEBIAN_FRONTEND=noninteractive apt-get purge -y --auto-remove linux-*
 fi
-sudo rm -rf /var/lib/apt/lists
+sudo rm -rf /var/lib/apt/lists && sudo apt-get clean
